504f5616c8dbbcf8cf84187b8b4c66f1a4d8a941
ilham majid zaiman <ilhammajidzaiman07@gmail.com>
2023-08-18 21:01:46 +0700

    feat: slideshow resource

---
+++ app/Filament/Resources/SlideshowResource.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use stdClass;
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Set;
+use Filament\Forms\Form;
+use App\Models\Slideshow;
+use Filament\Tables\Table;
+use Illuminate\Support\Str;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Forms\Components\FileUpload;
+use Filament\Tables\Columns\ToggleColumn;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\SlideshowResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\SlideshowResource\RelationManagers;
+
+class SlideshowResource extends Resource
+{
+    protected static ?string $model = Slideshow::class;
+    protected static ?string $navigationIcon = 'heroicon-o-photo';
+    protected static ?string $modelLabel = 'Tampilan slide';
+    protected static ?string $navigationLabel = 'Tampilan slide';
+    protected static ?string $slug = 'slideshow';
+    protected static ?string $recordTitleAttribute = 'title';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                // Forms\Components\TextInput::make('file')
+                //     ->required()
+                //     ->maxLength(255),
+                // Forms\Components\Toggle::make('is_active')
+                //     ->required(),
+
+                Section::make()
+                    ->columnSpan(2)
+                    ->schema([
+                        TextInput::make('title')
+                            ->label('Judul')
+                            ->maxLength(255)
+                            ->live(1)
+                            ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state))),
+                        TextInput::make('slug')
+                            ->label('Slug')
+                            ->maxLength(255)
+                            ->disabled()
+                            ->dehydrated(),
+                        Textarea::make('subtitle')
+                            ->label('Subtitle')
+                            ->maxLength(255)
+                            ->rows(3),
+                        FileUpload::make('file')
+                            ->label('File')
+                            ->required()
+                            ->maxSize(1024)
+                            ->directory('slideshows/' . date('Y/m'))
+                            ->image()
+                            ->imageEditor()
+                            ->openable()
+                            ->downloadable()
+                            ->helperText('Gambar harus diisi. Maksimal ukuran file 1024 kb atau 1 mb.'),
+                        Toggle::make('is_active')
+                            ->label('Status')
+                            ->required()
+                            ->default('1'),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+
+                TextColumn::make('index')
+                    ->label('#')
+                    ->state(
+                        static function (HasTable $livewire, stdClass $rowLoop): string {
+                            return (string) ($rowLoop->iteration +
+                                ($livewire->getTableRecordsPerPage() * ($livewire->getTablePage() - 1
+                                ))
+                            );
+                        }
+                    ),
+                TextColumn::make('id')
+                    ->label('Id')
+                    ->sortable()
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                ImageColumn::make('file')
+                    ->label('File')
+                    ->circular(),
+                TextColumn::make('title')
+                    ->label('Judul')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('slug')
+                    ->label('Slug')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('subtitle')
+                    ->label('Sub Judul')
+                    ->sortable()
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('created_at')
+                    ->label('Dibuat')
+                    ->dateTime()
+                    ->sortable(),
+                TextColumn::make('updated_at')
+                    ->label('Diperbarui')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('deleted_at')
+                    ->label('Dihapus')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                ToggleColumn::make('is_active')
+                    ->label('Status')
+                    ->sortable(),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSlideshows::route('/'),
+            'create' => Pages\CreateSlideshow::route('/create'),
+            'view' => Pages\ViewSlideshow::route('/{record}'),
+            'edit' => Pages\EditSlideshow::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
