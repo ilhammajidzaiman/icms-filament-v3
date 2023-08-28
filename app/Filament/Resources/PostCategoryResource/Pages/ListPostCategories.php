e0915bc1b4f610f00f79ffff57d36e0e845e9359
ilhammajidzaiman <ilhammajidzaiman07@gmail.com>
2023-08-28 13:32:42 +0700

    feat: add widget category

--- app/Filament/Resources/PostCategoryResource/Pages/ListPostCategories.php
+++ app/Filament/Resources/PostCategoryResource/Pages/ListPostCategories.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\PostCategoryResource\Pages;
 
 use App\Filament\Resources\PostCategoryResource;
+use App\Filament\Resources\PostCategoryResource\Widgets\CategoryOverview;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
@@ -14,6 +15,13 @@ class ListPostCategories extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            CategoryOverview::class,
         ];
     }
 }
---
+++ app/Filament/Resources/PostCategoryResource/Widgets/CategoryOverview.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\PostCategoryResource\Widgets;
+
+use App\Models\PostCategory;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+
+class CategoryOverview extends BaseWidget
+{
+    // protected static string $view = 'filament.resources.post-category-resource.widgets.category-overview';
+
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Semua', PostCategory::all()->count())
+                ->chart([PostCategory::all()->count()]),
+            Stat::make('Aktif', PostCategory::where('is_active', true)->count())
+                ->color('success'),
+            Stat::make('Sampah', PostCategory::where('deleted_at', false)->count())
+                ->color('warning'),
+        ];
+    }
+}
